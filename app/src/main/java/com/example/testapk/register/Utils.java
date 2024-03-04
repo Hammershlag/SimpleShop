7705cc8f0ad8b2fef674e11055b9fd5914f4cfd7
Hammershlag <35782145+Hammershlag@users.noreply.github.com>
2024-03-04 22:43:03 +0100

    Update Utils.java

--- app/src/main/java/com/example/testapk/register/Utils.java
+++ app/src/main/java/com/example/testapk/register/Utils.java
@@ -2,6 +2,6 @@ package com.example.testapk.register;
 
 public class Utils {
 
-    public static final  String EMAIL = "wysylaniemailijava@gmail.com";
-    public static final String PASSWORD = "Klamka125@";
+    public static final  String EMAIL = "...";
+    public static final String PASSWORD = "...";
 }
